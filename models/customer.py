c8cb5488b6cb2e103a433e6ea2928bda4ab6c94f
YoucefMesselem <messelemy@gmail.com>
2021-09-15 21:57:13 +0400

    Fixed the Draft invoice should not be calculated and the Credit note should to be negative

--- models/customer.py
+++ models/customer.py
@@ -10,11 +10,14 @@ class CustomerScore(models.Model):
     def _compute_total_scores(self):
         """Calculates the total score of a customer from his invoices"""
         for rec in self:
-            score_count = self.env['account.move'].search([('partner_id', '=', rec.id)])
+            customer_records = self.env['account.move'].search([('partner_id', '=', rec.id)])
             total_score = 0
-            for score in score_count:
-                name = score['name'].split('/')[0]
-                if name != "RINV":
-                    print(name)
-                total_score += score['customer_score']
+            for record in customer_records:
+                name = record['name'].split('/')[0]
+                print(name)
+                if name != '':
+                    if name != "RINV":
+                        total_score += record['customer_score']
+                    else:
+                        total_score -= record['customer_score']
             rec.sales_score = total_score
